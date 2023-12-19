fd39b31439f7552f5bd189124200bf1c4f98c269
Subhadip <mnhacker2001@gmail.com>
2023-12-19 18:41:27 +0530

    Add withCredentials option to axiosConfig

--- src/utils/axiosConfig.ts
+++ src/utils/axiosConfig.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const axiosConfig = axios.create({
   baseURL: "https://yoga-enroller-eb3lbyeafa-em.a.run.app",
+  withCredentials: false,
 });
 
 export default axiosConfig;
